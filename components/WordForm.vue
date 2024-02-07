ece3aabc3ac95a9fb7d583db5acf84b207f58a8a
Paulina Shakirova <paulinashakirova@gmail.com>
2024-02-07 21:50:22 +0100

    Revert "Revert "add VueUse""

--- components/WordForm.vue
+++ components/WordForm.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { useWordStore } from "@/stores/CardStore.js";
+import { useDateFormat } from "@vueuse/core";
 import Button from "primevue/button";
+import Calendar from "primevue/calendar";
 import InputText from "primevue/inputtext";
 import Textarea from "primevue/textarea";
+import { v4 as uuidv4 } from "uuid";
 import { ref } from "vue";
 
 const store = useWordStore();
@@ -12,27 +15,37 @@ const word = ref({
   name: "",
   pronunciation: "",
   definition: "",
+  dateAdded: "",
 });
 
 function addWord() {
   incrementID();
+  formatDate();
   const send = {
     id: word.value.id,
     name: word.value.name,
     pronunciation: word.value.pronunciation,
     definition: word.value.definition,
+    dateAdded: word.value.dateAdded,
   };
   store.addWord(send);
   clearForm();
 }
+
+function formatDate() {
+  const formatted = useDateFormat(word.value.dateAdded, "Do of MMM YYYY");
+  word.value.dateAdded = formatted.value;
+}
+
 function incrementID() {
-  word.value.id++;
+  word.value.id = uuidv4();
 }
 
 function clearForm() {
   word.value.name = "";
   word.value.pronunciation = "";
   word.value.definition = "";
+  word.value.dateAdded = "";
 }
 </script>
 
@@ -50,6 +63,10 @@ function clearForm() {
     <div class="flex">
       <label class="w-48">Definition</label>
       <Textarea class="w-full" autoResize v-model="word.definition" />
+    </div>
+    <div class="flex">
+      <label class="w-48">Date added</label>
+      <Calendar class="w-full" v-model="word.dateAdded" dateFormat="dd/mm/yy" />
     </div>
     <div class="flex ml-auto">
       <Button label="Add" class="px-10" @click="addWord" />
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,63 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWordStore } from "@/stores/CardStore.js";
+import Paginator from "primevue/paginator";
+
+const store = useWordStore();
+const router = useRouter();
+
+const dataSource = ref(store.words);
+const pageParams = ref({
+  perPage: 10,
+  page: 1,
+  first: 0,
+});
+
+const wordsCurrentPage = computed(() => {
+  return dataSource.value.slice(
+    pageParams.value.first,
+    pageParams.value.first + Number(pageParams.value.perPage)
+  );
+});
+
+function onChange(data: any) {
+  pageParams.value.perPage = Number(data.rows);
+  pageParams.value.page = data.page + 1;
+  pageParams.value.first = data.first;
+  router.push({
+    query: {
+      page: (data.page + 1).toString(),
+      perPage: data.rows.toString() ?? 23,
+    },
+  });
+}
+
+const paginate = (page: number, perPage?: number) => {
+  router.push({
+    query: {
+      page: page.toString(),
+      perPage: perPage?.toString() ?? 23,
+    },
+  });
+};
+
+onBeforeMount(() => {
+  dataSource.value = store.words;
+  paginate(pageParams.value.page, pageParams.value.perPage);
+});
+</script>
 
 <template>
   <div class="m-8 md:m-16 flex flex-col items-center">
-    <CardList />
+    <CardList :words-current-page="wordsCurrentPage" />
+    <Paginator
+      class="mx-auto"
+      :rows="10"
+      :totalRecords="store.wordsCount"
+      template="FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink RowsPerPageDropdown"
+      currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
+      :rowsPerPageOptions="[10, 20, 30]"
+      v-on:page="onChange($event)"
+    />
     <WordForm />
   </div>
 </template>
